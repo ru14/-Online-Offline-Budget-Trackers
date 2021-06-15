e4c72fe8f23b3ea4a2edab90e0cfa69dea22dbaa
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-15 13:20:58 -0700

    package have some chacges

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
-const PORT = 3000;
+
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,19 +18,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetdb", { useNewUrlParser: true});
+const db = mongoose.connection;
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
 
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
